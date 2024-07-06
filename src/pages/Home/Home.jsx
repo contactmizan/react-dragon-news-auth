1aece2d3608ac5cb404a6fdeac7586becf47c961
contactmizan <contactmizan1206@gmail.com>
2024-07-06 23:03:22 +0600

    create page different pages layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
+import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is Home</h2>
+            <Header></Header>
+            <Navbar></Navbar>
+           
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2">
+                    <h2 className="text-4xl">News coming soon</h2>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 }
