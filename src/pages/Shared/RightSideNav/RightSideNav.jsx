a181454238abf9940e7bf88d75e953228ca2bab5
contactmizan <contactmizan1206@gmail.com>
2024-07-09 01:46:44 +0600

    load news category from json file

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,12 @@
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
+import qZone1 from "../../../assets/qZone1.png"
+import qZone2 from "../../../assets/qZone2.png"
+import qZone3 from "../../../assets/qZone3.png"
 
 const RightSideNav = () => {
     return (
         <div>
-            <div className="p-4 space-y-3">
+            <div className="p-4 space-y-3 mb-6">
                 <h2 className="text-3xl">Login With</h2>
 
                 <button className="btn  btn-outline w-full">
@@ -15,8 +18,31 @@ const RightSideNav = () => {
                     <FaGithub></FaGithub>
                     Login with Github
                 </button>
-
             </div>
+
+            <div className="p-4 mb-6">
+                <h2 className="text-3xl mb-4">Find Us On</h2>
+                <a className="p-4 flex items-center text-lg border rounded-t-lg" href="">
+                    <FaFacebook className="mr-3"></FaFacebook>
+                    Facebook
+                </a>
+                <a className="p-4 flex items-center text-lg border-x" href="">
+                    <FaTwitter className="mr-3"></FaTwitter>
+                    Twitter
+                </a>
+                <a className="p-4 flex items-center text-lg border rounded-b-lg" href="">
+                    <FaInstagram className="mr-3"></FaInstagram>
+                    Instagram
+                </a>
+            </div>
+            {/* Q zone */}
+            <div className="p-4 space-y-3 mb-6 bg-[#F3F3F3]">
+                <h2 className="text-3xl">Q-Zone</h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
+
         </div>
     );
 }
